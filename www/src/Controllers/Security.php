b9117fb297b7df6d846a49d2058354af611969c2
Wendy Afrim <wendy.afrim2@gmail.com>
2021-09-08 22:38:39 +0200

    Correctif - Récupération de la page login ok

--- www/src/Controllers/Security.php
+++ www/src/Controllers/Security.php
@@ -7,10 +7,7 @@ use App\Core\View;
 class Security {
 
     public function login() {
-        // echo "page login";
-        // die();
-        $view = new View('Security/login', 'front-template');
 
-        // $view = $view->renderHtml();
+        $view = new View('Security/login', 'front-template');
     }
 }
--- www/src/Views/Templates/front-template.php
+++ www/src/Views/Templates/front-template.php
@@ -1,3 +1,10 @@
-<?php 
-
-//  Template front (Qui reprendra tous les élements fixes d'une page = Header & Footer)
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Sidebar</title>
+    <meta name="description" content="Figma htmlGenerator">
+    <meta name="author" content="htmlGenerator">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+    <link rel="stylesheet" href="../../../dist/css/main.css">
+</head>
+<?php require $this->view ;?>
