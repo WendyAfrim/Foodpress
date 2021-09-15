6f641cc27cf7a60996fe91a6aaa85d9a7e3894fd
WendyAfrim <wendy.afrim2@gmail.com>
2021-09-15 20:34:25 +0200

    Implémentation Singleton - Connexion à la bdd

--- www/src/Core/Database.php
+++ www/src/Core/Database.php
@@ -1,3 +1,47 @@
-<?php 
+<?php
 
-// Ceci est un helper qui sera destiné à l'interaction avec la base de données.
+namespace App\Core;
+
+use Exception;
+
+class Database
+{
+    protected static $instance = null;
+
+    protected $host = 'localhost';
+    protected $user = 'root';
+    protected $password = 'root';
+    protected $dbname = 'foodpress';
+    protected $port = '3306';
+
+    public function __construct()
+    {
+        $this->conn = new \PDO("mysql:host=database;dbname=mvcdocker2;port=3306","root","password");
+
+        // $this->conn = new \PDO('mysql:host='.$this->host.';dbname='.$this->dbname.';port='.$this->port,$this->user, $this->password);
+
+        $this->conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        $this->conn->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
+        try {
+
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    public static function getInstance() 
+    {
+        if(is_null(self::$instance)) {
+            
+            self::$instance = new Database();
+        }
+
+        return self::$instance;
+    }
+
+    public function getConnection() 
+    {
+        return $this->conn;
+    }
+
+}
