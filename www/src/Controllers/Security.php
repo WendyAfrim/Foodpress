0b3145d9092e479a83b73099e760e51e550e84dc
WendyAfrim <wendy.afrim2@gmail.com>
2021-09-15 20:34:25 +0200

    Implémentation Singleton - Connexion à la bdd

--- www/src/Controllers/Security.php
+++ www/src/Controllers/Security.php
@@ -1,13 +1,33 @@
-<?php 
+<?php
 
 namespace App\Controllers;
 
+use App\Core\Database;
 use App\Core\View;
 
-class Security {
+class Security
+{
 
-    public function login() {
+    public function login()
+    {
 
         $view = new View('Security/login', 'front-template');
+    }
+
+    public function register()
+    {
+
+        $instance = Database::getInstance();
+        $conn = $instance->getConnection();
+
+        
+		$classExploded = explode("\\", get_called_class());
+		$table = end($classExploded) ;
+
+		$columns = get_object_vars($this);
+		$toDelete = get_class_vars(get_class());
+		$data = array_diff_key($columns, $toDelete);
+
+
     }
 }
