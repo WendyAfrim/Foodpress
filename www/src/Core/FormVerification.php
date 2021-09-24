3646fb25e76648a2fc7d079695677b69d0cf4cdb
Florian Perrier <florian@lagenceoueb.tech>
2021-09-24 11:41:38 +0200

    Correction de la méthode checkUnicity()

--- www/src/Core/FormVerification.php
+++ www/src/Core/FormVerification.php
@@ -55,6 +55,10 @@ class FormVerification
             if (isset($inputRules['unicity']) && $inputRules['unicity']) {
                 FormVerification::checkUnicity($inputKey, $inputValue, $table);
             }
+
+            if (isset($inputRules['unicity']) && $inputRules['unicity']) {
+                FormVerification::checkUnicity($inputKey, $inputValue, $table);
+            }
         }
         return self::$array_errors;
         // var_dump(self::$array_errors); 
