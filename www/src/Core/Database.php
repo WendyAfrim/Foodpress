4da113fc1d44bc4f8a2239fbcac98b622c818bc3
WendyAfrim <wendy.afrim2@gmail.com>
2021-09-29 18:39:52 +0200

    Refactorisation du singleton Database

--- www/src/Core/Database.php
+++ www/src/Core/Database.php
@@ -3,77 +3,57 @@
 namespace App\Core;
 
 use Exception;
+use PDO;
 
 class Database
 {
     private static $instance = null;
 
-    protected function __construct()
+    public static function getPdo(): PDO
     {
-        try {
-            
-            self::$instance = new \PDO("mysql:host=database;dbname=mvcdocker2;port=3306","root","password");
-            self::$instance->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-            self::$instance->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
-            self::$instance->query('SET NAMES utf8');
-            self::$instance->query('SET CHARACTER SET utf8');
+        if (self::$instance === null) {
 
-        } catch (Exception $e) {
-            echo $e->getMessage();
-        }
-    }
-
-    /**
-     * Instanciation de la class Database de manière unique
-     * @static
-     * @return object 
-     */
-    public static function getInstance() 
-    {
-        if(is_null(self::$instance)) {
-            
-            self::$instance = new Database();
+            self::$instance = new PDO("mysql:host=database;dbname=mvcdocker2;port=3306", "root", "password", [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ]);
         }
 
         return self::$instance;
     }
-
 
     /**
      * Enregistrement dynamique d'un objet en base de données
      *
      * @return void
      */
-    public function save() {
+    public function save()
+    {
 
         $classExploded = explode("\\", get_called_class());
-		$table = end($classExploded) ;
-		
+        $table = end($classExploded);
+
         $columns = get_object_vars($this);
-		$toDelete = get_class_vars(get_class());
+        $toDelete = get_class_vars(get_class());
         $data = array_diff_key($columns, $toDelete);
         unset($data['conn']);
 
-        
+
         if (is_null($this->getId())) {
-            
-            
+
+
             $sql = " INSERT INTO $table 
-			(". implode(",", array_keys($data)) .") 
+			(" . implode(",", array_keys($data)) . ") 
 			VALUES 
-            (:". implode(",:", array_keys($data)) .")";
-            
+            (:" . implode(",:", array_keys($data)) . ")";
+
             $conn = self::getInstance();
 
-			$queryPrepared = $conn->prepare($sql);
+            $queryPrepared = $conn->prepare($sql);
 
-			$queryPrepared->execute($data);
-			
-		}else {
-			//UPDATE
-		}
-
-
+            $queryPrepared->execute($data);
+        } else {
+            //UPDATE
+        }
     }
-
 }
