6da7dff06230237c749423bec8d7439c789c2c34
Wendy Afrim <wendy.afrim2@gmail.com>
2021-10-11 12:36:15 +0200

    Mise en place du fichier .env

--- www/src/Core/Database.php
+++ www/src/Core/Database.php
@@ -5,15 +5,20 @@ namespace App\Core;
 use Exception;
 use PDO;
 
+require 'vendor/autoload.php';
+
 class Database
 {
     private static $instance = null;
 
     public static function getPdo(): PDO
     {
+        $dotenv = \Dotenv\Dotenv::createImmutable(dirname(__DIR__, 2));
+        $dotenv->load();
+
         if (self::$instance === null) {
 
-            self::$instance = new PDO("mysql:host=database;dbname=mvcdocker2;port=3306", "root", "password", [
+            self::$instance = new PDO("mysql:host=".$_ENV['DB_HOST'].";dbname=".$_ENV['DB_NAME'].";port=".$_ENV['DB_PORT']."", "".$_ENV['DB_USER']."", "".$_ENV['DB_PASSWORD']."", [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
             ]);
