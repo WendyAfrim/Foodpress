3db43e0efd34f002ffec6f31fc491d3e024ee120
Wendy Afrim <wendy.afrim2@gmail.com>
2021-11-04 22:07:12 +0100

    [Admin] - Form ajout d'un nouveau compte admin - Nommage des routes /admin - Helpers GeneratorPassword créer

--- www/src/Controllers/AdminController.php
+++ www/src/Controllers/AdminController.php
@@ -2,13 +2,22 @@
 
 namespace App\Controllers;
 
+use App\Core\DateGenerator;
+use App\Core\FormVerification;
+use App\Core\PasswordGenerator;
 use App\Core\View;
+use App\Form\AccountForm;
+use App\Helpers\Generator;
 use App\Models\User;
 
 class AdminController
 {
-
-    public function all_accounts()
+    /**
+     * List all the administrator accounts
+     *
+     * @return void
+     */
+    public function all_admin_accounts()
     {
         $user = new User();
 
@@ -17,5 +26,44 @@ class AdminController
         $view = new View('Admin/index', 'back-template');
         $view->title = 'Foodpress | Tous les comptes';
         $view->users = $users;
+    }
+
+    /**
+     * Add an admin account in database
+     *
+     * @return void
+     */
+    public function add_admin_account(): void
+    {
+        $user = new User();
+
+        $form = new AccountForm();
+        $config = $form->createForm();
+
+        $date = Generator::generateDate();
+        $randomPassword = Generator::generatePassword(8);
+
+        $errors = "";
+        if (!empty($_POST)) {
+
+            $errors = FormVerification::check($_POST, $config);
+
+            if (!$errors) {
+
+                $user->setFirstname($_POST['firstname']);
+                $user->setLastname($_POST['lastname']);
+                $user->setEmail($_POST['email']);
+                $user->setRoles("['ROLE_" . strtoupper($_POST['roles'] . "']"));
+                $user->setPassword($randomPassword);
+                $user->setCreatedAt($date);
+
+                $user->save();
+            }
+        }
+
+        $view = new View('Admin/add_user', 'back-template');
+        $view->form = $form->renderHtml();
+        $view->title = 'Formulaire | Ajout d\'un compte';
+        $view->$errors = $errors;
     }
 }
---
+++ www/src/Form/AccountForm.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Form;
+
+use App\AbstractClass\AbstractForm;
+
+class AccountForm extends AbstractForm
+{
+    protected $config;
+    protected $html;
+
+    public function __construct()
+    {
+        $config = $this->createForm();
+        parent::__construct($config);
+        $this->renderHtml();
+    }
+
+    public function createForm()
+    {
+        return [
+            "method" => "POST",
+            "form-title" => 'Formulaire | Ajout d\'un compte',
+            "form-id" => 'account-form',
+            "submit" => "Enregistrer",
+            "inputs" => [
+                'lastname' => [
+                    'label' => 'Nom',
+                    'type' => 'text',
+                    'required' => true,
+                    'placeholder' => '',
+                    "minLength" => 2,
+                    'class' => ['large' => 'col-lg-6', 'medium' => 'col-md-6', 'small' => 'col-xs-12'],
+                    'row' => 'start',
+                    "error" => "Le champ nom doit faire au minimum 2 caractères"
+                ],
+                'firstname' => [
+                    'label' => 'Prénom',
+                    'type' => 'text',
+                    'required' => true,
+                    'placeholder' => '',
+                    "minLength" => 2,
+                    'class' => ['large' => 'col-lg-6', 'medium' => 'col-md-6', 'small' => 'col-xs-12'],
+                    'row' => 'end',
+                    "error" => "Le champ nom doit faire au minimum 2 caractères"
+                ],
+                'email' => [
+                    'label' => 'Email',
+                    'type' => 'email',
+                    'required' => true,
+                    'placeholder' => 'Exemple : contact@foodpress.com',
+                    "unicity" => "email",
+                    'class' => ['large' => 'col-lg-12', 'medium' => 'col-md-12', 'small' => 'col-xs-12'],
+                    'row' => 'start_end',
+                    "error" => "Le champ email n'est pas conforme"
+                ],
+                'roles' => [
+                    'label' => 'Role',
+                    'type' => 'select',
+                    'options' => ["Administrateur", "Editeur"],
+                    'required' => true,
+                    'placeholder' => '',
+                    "minLength" => 2,
+                    'class' => ['large' => 'col-lg-12', 'medium' => 'col-md-12', 'small' => 'col-xs-12'],
+                    'row' => 'start_end',
+                    'error' => 'Le ch'
+                ],
+            ]
+        ];
+    }
+}
