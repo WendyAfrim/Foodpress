86bc5a81d586a255d4d29f5d30332cb22c7cdcbb
Wendy Afrim <wendy.afrim2@gmail.com>
2021-11-26 11:13:06 +0100

    [Modal] Perfectionnectement de la partie delete + modal - Delete appliqué pour les comptes admin

--- www/src/Views/Ajax/modal_delete_admin.php
+++ www/src/Views/Ajax/modal_delete_admin.php
@@ -3,9 +3,9 @@
     <div class="btn-close" data-close-button="#modal">&times;</div>
 </div>
 <div class="modal-body">
-    Etes-vous sur de vouloir supprimer ce client ?
+    Etes-vous sur de vouloir supprimer ce compte ?
 </div>
 <div class="modal-footer">
-    <button class='btn_modal btn_modal--danger btn-delete' data-ajax-class="user" data-url='/admin/clients'>Oui</button>
+    <button id="#btn-delete" class='btn_modal btn_modal--danger btn-delete' data-ajax-class='user' data-url='/admin/accounts'>Oui</button>
     <button class="btn_modal btn-close">Non</button>
 </div>
