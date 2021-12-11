640b51ded00401de59be77b274e6d354deb019ea
WendyAfrim <wendy.afrim2@gmail.com>
2021-12-11 23:17:19 +0100

    [Fix] - Ajout du design au form login client  avec config form

--- www/src/Views/Security/login.php
+++ www/src/Views/Security/login.php
@@ -5,20 +5,9 @@
         <?php endforeach ?>
     </ul>
 <?php endif; ?>
-
 <div id="form-login">
-    <div class="container">
+    <div class="wrapper">
         <img src="https://images.unsplash.com/photo-1493770348161-369560ae357d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80" alt="Food image">
-        <div class="formLogin">
-            <form action="" method="POST">
-                <h2>Connexion</h2>
-                <label for="username">Nom d'utilisateur</label>
-                <input type="text" name="username" id="username">
-                <label for="password">Mot de passe</label>
-                <input type="password" name="password" id="password">
-                <button type="submit">Se connecter</button>
-            </form>
-        </div>
-
+        <?= $form ?>
     </div>
 </div>
