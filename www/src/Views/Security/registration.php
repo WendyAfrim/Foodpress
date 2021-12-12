b83444278aa68a015d63dc1ca41c913798c1870b
WendyAfrim <wendy.afrim2@gmail.com>
2021-12-12 12:45:53 +0100

    [Front] - Intégration du formulaire d'inscription

--- www/src/Views/Security/registration.php
+++ www/src/Views/Security/registration.php
@@ -1,9 +1,15 @@
-<?php if (isset($errors)): ?>
+<?php if (isset($errors)) : ?>
     <ul>
-        <?php foreach ($errors as $error): ?>
+        <?php foreach ($errors as $error) : ?>
             <li><?= $error ?></li>
         <?php endforeach ?>
     </ul>
 <?php endif; ?>
-
-<?= $form; ?>
+<div id="form-register">
+    <div class="image">
+        <img src="https://images.unsplash.com/photo-1484723091739-30a097e8f929?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=898&q=80" alt="">
+    </div>
+    <div class="wrapper">
+        <?= $form; ?>
+    </div>
+</div>
