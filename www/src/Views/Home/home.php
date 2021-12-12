455c7022a605e5b947406787c11219bf81e6bed0
WendyAfrim <wendy.afrim2@gmail.com>
2021-12-13 00:22:01 +0100

    [Homepage] - Intégration de la page d'accueil

--- www/src/Views/Home/home.php
+++ www/src/Views/Home/home.php
@@ -1,57 +1,46 @@
 <body class="homepage">
     <!-- Home section -->
     <section class="home" id="home">
-        <div class="home-slider">
-            <div class="container">
-                <div class="sliders">
-                    <!-- <div class="slide">
-                        <div class="content">
-                            <span>Our special dishes</span>
-                            <h3>Spicy noodles</h3>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum dicta ipsam earum esse animi fugiat mollitia suscipit distinctio laudantium numquam quasi repellat asperiores consequatur, facere dolor assumenda nisi error facilis?</p>
-                            <a href="" class="btn">order now</a>
+        <div class="container">
+            <div class="wrapper">
+                <div class="banner">
+                    <div class="content">
+                        <div class="content-title">
+                            <h1>Foodpress</h1>
                         </div>
-                        <div class="image">
-                            <img src="https://img.freepik.com/free-photo/spaghetti-bolognese-plate-blue_119015-53.jpg?size=338&ext=jpg" alt="Noodles" class="active">
+                        <div class="content-description">
+                            <p>Votre outil clé en main pour concevoir votre site internet. <br>
+                                N'attendez plus et contactez-nous pour obtenir plus d'informations !</p>
                         </div>
                     </div>
-                    <div class="slide">
-                        <div class="content">
-                            <span>Our special dishes</span>
-                            <h3>Fried chicken</h3>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum dicta ipsam earum esse animi fugiat mollitia suscipit distinctio laudantium numquam quasi repellat asperiores consequatur, facere dolor assumenda nisi error facilis?</p>
-                            <a href="" class="btn">order now</a>
-                        </div>
-                        <div class="image">
-                            <img src="https://img.freepik.com/free-photo/top-view-fried-chicken-drumstick-with-ketchup_23-2148682831.jpg?size=338&ext=jpg&ga=GA1.2.1548854029.1638122541" alt="Noodles">
-                        </div>
-                    </div>
-                    <div class="slide">
-                        <div class="content">
-                            <span>Our special dishes</span>
-                            <h3>Hot pizza</h3>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum dicta ipsam earum esse animi fugiat mollitia suscipit distinctio laudantium numquam quasi repellat asperiores consequatur, facere dolor assumenda nisi error facilis?</p>
-                            <a href="" class="btn">order now</a>
-                        </div>
-                        <div class="image">
-                            <img src="https://image.freepik.com/free-photo/pizza-pizza-filled-with-tomatoes-salami-olives_140725-1200.jpg" alt="Noodles">
-                        </div>
-                    </div>
-                </div> -->
-
-                    <img src="https://img.freepik.com/free-photo/spaghetti-bolognese-plate-blue_119015-53.jpg?size=338&ext=jpg" alt="Noodles" class="active">
-
-                    <img src="https://img.freepik.com/free-photo/top-view-fried-chicken-drumstick-with-ketchup_23-2148682831.jpg?size=338&ext=jpg&ga=GA1.2.1548854029.1638122541" alt="Noodles">
-                    <img src="https://image.freepik.com/free-photo/pizza-pizza-filled-with-tomatoes-salami-olives_140725-1200.jpg" alt="Noodles">
-                    <div class="sliders-buttons">
-                        <input type="button" class="previous-button button button--small button--blue" value="Previous">
-                        <input type="button" class="start-button button button--small button--lightviolet" value="Start">
-                        <input type="button" class='next-button button button--small button--blue' value="Next">
+                    <div class="image">
                     </div>
                 </div>
             </div>
+        </div>
+        </div>
     </section>
     <!-- Home section ends -->
     <!-- About section start -->
+    <section class="about" id="about">
+        <div class="heading">Pourquoi nous choisir ?</div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor asperiores deleniti voluptas placeat quisquam quis, odio optio ea ad laboriosam sequi voluptatum, reprehenderit eos repellat distinctio sapiente ex obcaecati. Omnis?</p>
+        <div class="content">
+            <div class="icons-container">
+                <div class="icons">
+                    <div class="fas fa-shipping-fast"></div>
+                    <span>Livraison gratuite</span>
+                </div>
+                <div class="icons">
+                    <div class="fas fa-dollar-sign"></div>
+                    <span>Paiement facile</span>
+                </div>
+                <div class="icons">
+                    <div class="fas fa-headset"></div>
+                    <span>Service 7/7j</span>
+                </div>
+            </div>
+        </div>
+    </section>
     <!-- About section ends -->
 </body>
