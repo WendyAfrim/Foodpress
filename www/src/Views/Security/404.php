3cd23dd2345e60a45ed1048cb4ce627893c20761
Wendy Afrim <wendy.afrim2@gmail.com>
2021-12-13 21:43:53 +0100

    [404] - Intégration de la page 404

--- www/src/Views/Security/404.php
+++ www/src/Views/Security/404.php
@@ -1,1 +1,7 @@
-Erreur 404 : page introuvable
+<div class="error-404">
+    <h1>4<span>0</span>4</h1>
+    <h3>Oops! Il semble que tu ais perdu ton chemin :)</h3>
+    <div class="button">
+        <a href="/" class="btn btn--white cta-home">Revenir à l'accueil</a>
+    </div>
+</div>
