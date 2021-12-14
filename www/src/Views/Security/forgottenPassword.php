225f3411bef95c1c482a1cef0f364f8eb3817427
Wendy Afrim <wendy.afrim2@gmail.com>
2021-12-14 11:42:31 +0100

    Fix css

--- www/src/Views/Security/forgottenPassword.php
+++ www/src/Views/Security/forgottenPassword.php
@@ -6,7 +6,7 @@
     </ul>
 <?php endif; ?>
 <div id="form-login">
-    <div class="wrapper" style="height:100vh; margin-top:10rem;">
+    <div class="wrapper">
         <img src="https://images.unsplash.com/photo-1470337458703-46ad1756a187?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80" alt="Food image">
         <?= $form ?>
     </div>
--- www/src/Views/Security/new_password.php
+++ www/src/Views/Security/new_password.php
@@ -6,7 +6,7 @@
     </ul>
 <?php endif; ?>
 <div id="reset-password">
-    <div class="wrapper">
+    <div class="wrapper" style="height:100vh; margin-top:10rem;">
         <?= $form ?>
     </div>
 </div>
